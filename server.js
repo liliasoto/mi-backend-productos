e6960ba44bb44de1b4ab21f6d9af769618d185bd
liliasoto <0.lilia.soto.0@gmail.com>
2024-11-19 12:22:45 -0600

    cambios para render

--- server.js
+++ server.js
@@ -69,4 +69,6 @@ app.delete('/api/products/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
