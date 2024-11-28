2860660e22fb8d5085f194536a239969c302fe7b
liliasoto <0.lilia.soto.0@gmail.com>
2024-11-28 12:50:45 -0600

    para lo de androir

--- server.js
+++ server.js
@@ -23,11 +23,14 @@ mongoose.connect(process.env.MONGODB_URI, {
 // Importar rutas
 const productRoutes = require('./productRoutes');
 const userRoutes = require('./userRoutes');
-
+const usuariosRoutes = require('./usuariosRoutes');
+const medicionRoutes = require('./medicionRoutes');
 
 // Usar rutas
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/', usuariosRoutes);
+app.use('/', medicionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
